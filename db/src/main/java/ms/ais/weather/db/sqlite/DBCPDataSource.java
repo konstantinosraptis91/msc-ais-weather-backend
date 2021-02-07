afc3ddd5e513b8b07c16bf38dbe396ab1061227b
Konstantinos <kraptis@unipi.gr>
2021-02-07 20:16:49 +0200

    sqlite conf implemented

--- db/src/main/java/ms/ais/weather/db/sqlite/DBCPDataSource.java
+++ db/src/main/java/ms/ais/weather/db/sqlite/DBCPDataSource.java
@@ -1,8 +1,14 @@
 package ms.ais.weather.db.sqlite;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
+import com.typesafe.config.ConfigFactory;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.lang3.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -11,28 +17,46 @@ import java.sql.SQLException;
  */
 public class DBCPDataSource {
 
-    private static final BasicDataSource ds = new BasicDataSource();
+    private static final Logger LOGGER = LoggerFactory.getLogger(DBCPDataSource.class);
+    private static final BasicDataSource DATA_SOURCE = new BasicDataSource();
 
     static {
-        ds.setUrl("jdbc:sqlite:" + SystemUtils.getUserHome()
-            + "/sqlite/db/ms-ais-weather-schema.db");
-//        ds.setUrl("jdbc:sqlite:" + SystemUtils.getUserHome()
-//            + "/sqlite/ms-ais-weather-schema.db");
-//        ds.setUrl("jdbc:sqlite:" + SystemUtils.getUserHome()
-//            + "/Documents/SqliteDB/ms-ais-weather-schema.db");
-        // ds.setUsername("");
-        // ds.setPassword("");
-        ds.setMinIdle(5);
-        ds.setMaxIdle(10);
-        ds.setTimeBetweenEvictionRunsMillis(100);
-        ds.setMaxOpenPreparedStatements(100);
+        final String confFileName = "sqlite.conf";
+        final String confFilePath = SystemUtils.getUserDir().getParentFile().getParent()
+            + "/" + confFileName;
+
+        try {
+            final Config config = ConfigFactory.parseFile(new File(confFilePath)).resolve();
+            final String dbName = config.getConfig("sqlitedb").getString("dbName");
+            final String dbPath = config.getConfig("sqlitedb").getString("dbPath");
+
+            LOGGER.info("DB Name: " + dbName);
+            LOGGER.info("DB Path: " + dbPath);
+
+            DATA_SOURCE.setUrl("jdbc:sqlite:" + new File(dbPath));
+            // DATA_SOURCE.setUsername("");
+            // DATA_SOURCE.setPassword("");
+            DATA_SOURCE.setMinIdle(5);
+            DATA_SOURCE.setMaxIdle(10);
+            DATA_SOURCE.setTimeBetweenEvictionRunsMillis(100);
+            DATA_SOURCE.setMaxOpenPreparedStatements(100);
+        } catch (ConfigException e) {
+            final String sqliteConfFileStructure = "sqlitedb {" + System.lineSeparator()
+                + "\t dbName: \"the-db-name.db\"" + System.lineSeparator()
+                + "\t dbPath: \"the-db-path\"" + System.lineSeparator()
+                + "}";
+            LOGGER.error("Unable to find sqlite.conf file in: " + confFilePath);
+            LOGGER.error("Please add sqlite.conf in path with the following structure: "
+                + System.lineSeparator()
+                + sqliteConfFileStructure);
+        }
     }
 
     private DBCPDataSource() {
     }
 
     public static Connection getConnection() throws SQLException {
-        return ds.getConnection();
+        return DATA_SOURCE.getConnection();
     }
 
 }
